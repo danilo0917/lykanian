33837c34d80694ff4d59cd2dd3f63286d78981a6
Tokarev Pavlovich <tokarevpavlovich0917@gmail.com>
2022-05-07 15:41:44 +1000

    commit

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react'
+import { useWallet } from '@binance-chain/bsc-use-wallet'
+import { allLanguages } from 'config/localisation/languageCodes'
+import { LanguageContext } from 'contexts/Localisation/languageContext'
+import useTheme from 'hooks/useTheme'
+import { usePriceCakeBusd } from 'state/hooks'
+import { Menu as UikitMenu } from '@pancakeswap-libs/uikit'
+import config from './config'
+
+const Menu = (props) => {
+  const { account, connect, reset } = useWallet()
+  const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
+  const { isDark, toggleTheme } = useTheme()
+  const cakePriceUsd = usePriceCakeBusd()
+
+  return (
+    <UikitMenu
+      account={account}
+      login={connect}
+      logout={reset}
+      isDark={isDark}
+      toggleTheme={toggleTheme}
+      currentLang={selectedLanguage && selectedLanguage.code}
+      langs={allLanguages}
+      setLang={setSelectedLanguage}
+      cakePriceUsd={cakePriceUsd.toNumber()}
+      links={config}
+      priceLink="https://poocoin.app/tokens/0x9477477CdDC4A05419A402A9754725Bc9Ee6a40e"
+      // to="bsc"
+      {...props}
+    />
+  )
+}
+
+export default Menu
